11d3da5150644dac20efe5c841d00684a476e85e
jayalakshmi-ede <jayalakshmi.ede@copart.com>
2026-01-20 12:11:25 +0530

    Added spacing changes

--- src/services/api.js
+++ src/services/api.js
@@ -8,9 +8,9 @@ const api = axios.create({
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-},
-(error) => Promise.reject(error)
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 api.interceptors.response.use(
