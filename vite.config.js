d3ba2677e41888004bc534148a26fdb4a84f4d3a
jayalakshmi-ede <jayalakshmi.ede@copart.com>
2026-01-21 12:08:36 +0530

    "Added PWA configuration"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Chicken Shop',
+        short_name: 'ChickenShop',
+        theme_color: '#d32f2f'
+      }
+    })
+  ]
 })
